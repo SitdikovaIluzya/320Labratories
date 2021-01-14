7657a8e7a72c50ffa58e89d92d0576d9f92ab213
sitdikova <ilsi.mazitowa@yandex.ru>
2021-01-14 13:15:59 +0300

    start 14.01.2021

---
+++ Laboratories320/Unit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320
+{
+    abstract class Unit
+    {
+        public string Name { get; set; }
+    }
+}

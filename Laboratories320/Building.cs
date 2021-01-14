97584e0967959b84472bc7816ec992cf56f9488f
zalina <zalina.davletkirowa@yandex.ru>
2021-01-14 13:19:52 +0300

    start 14.01.2021

--- Laboratories320/Building.cs
+++ Laboratories320/Building.cs
@@ -6,13 +6,13 @@ namespace Laboratories320
 {
     class Building
     {
-        public int CapacityPeople { get; set; }
-        public int CapacityObject { get; set; }
+        //    public int CapacityPeople { get; set; }
+        //    public int CapacityObject { get; set; }
 
-        public Building(int capacityPeople, int capacityObject)
-        {
-            this.CapacityPeople = capacityPeople;
-            this.CapacityObject = capacityObject;
-        }
+        //    public Building(int capacityPeople, int capacityObject)
+        //    {
+        //        this.CapacityPeople = capacityPeople;
+        //        this.CapacityObject = capacityObject;
+        //    }
     }
 }

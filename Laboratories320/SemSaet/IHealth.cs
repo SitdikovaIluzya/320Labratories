b5fbfa30b0a1637c292046981379bdf9d4f6c065
Elizkins <semenovaeliza7@gmail.com>
2021-01-14 13:12:18 +0300

    end 14.01.21

---
+++ Laboratories320/SemSaet/IHealth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.SemSaet
+{
+    interface IHealth
+    {
+        int Helth { get; set; }
+
+        void Health();
+    }
+}

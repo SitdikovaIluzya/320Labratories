5f0a41d10f95df02767655133ca25824a24f374c
Hamich <ihamatullin@gmail.com>
2021-01-17 10:39:31 +0300

    base classes and interface implemented

---
+++ Laboratories320/Hamatullin/1praktik/Interface/IConstructed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Hamatullin._1praktik.Interface
+{
+    interface IConstructed
+    {
+        int Width { get; set; }
+        int Length { get; set; }
+    }
+}
--- Laboratories320/Hamatullin/1praktik/UnitBuilding.cs
+++ Laboratories320/Hamatullin/1praktik/UnitBuilding.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using Laboratories320.Hamatullin._1praktik.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Laboratories320.Hamatullin._1praktik
 {
-    abstract class UnitBuilding : Unit
+    abstract class UnitBuilding : Unit,IConstructed
     {
-
-
-        public UnitBuilding(string Name,int Hp) : base (Name,Hp)
+        public UnitBuilding(string Name,int Hp,int Width,int Length) : base (Name,Hp)
         {
-
+            this.Width = Width;
+            this.Length = Length;
         }
+
+        public int Width { get; set; }
+        public int Length { get; set; }
     }
 }

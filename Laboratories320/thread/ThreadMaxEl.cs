03b4889c5605fb011e8b0ddbeee878571a37a5ae
ilinasaetova <saetova_i@list.ru>
2021-01-18 09:29:44 +0300

    18.01.21 end of pair

---
+++ Laboratories320/thread/ThreadMaxEl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.thread
+{
+    class ThreadMaxEl
+    {
+        public static void Max(double[] arr)
+        {
+            double max;
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if(arr[i]>arr[i-1])
+                {
+                    max = arr[i];
+                }
+            }
+        }
+    }
+}
+

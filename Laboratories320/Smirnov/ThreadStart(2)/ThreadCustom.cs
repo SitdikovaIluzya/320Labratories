1ae1c3e58b03a136f48b6b3897bce4fbafa42ccb
chestnut <alexandrrrrrr0702@gmail.com>
2021-01-18 21:13:34 +0300

    End of practice "ThreadStart(2)"

--- Laboratories320/Smirnov/ThreadStart(2)/ThreadCustom.cs
+++ Laboratories320/Smirnov/ThreadStart(2)/ThreadCustom.cs
@@ -19,11 +19,12 @@ namespace Laboratories320.Smirnov.ThreadStart_2_
             static void SumInArray()
             {
                 int sum = 0;
-                for (int i = 0; i < array.Length - 1; i++)
+                for (int i = 0; i < array.Length; i++)
                 {
                     sum += array[i];
                 }
-                Console.WriteLine("Sum" + sum);
+                Console.WriteLine("Sum: " + sum);
+                Console.WriteLine();
             }
             static void MaxInArray()
             {
@@ -35,16 +36,17 @@ namespace Laboratories320.Smirnov.ThreadStart_2_
                         max = array[i];
                     }
                 }
-                Console.WriteLine("Max" + max);
+                Console.WriteLine("Max: " + max);
             }
             static void SortInArray()
             {
+                Thread.Sleep(100);
+                Console.WriteLine("Отсоритрованный массив:");
                 Array.Sort(array);
                 for (int i = 0; i < array.Length; i++)
                 {
-                    Console.Write(array[i]);
+                    Console.Write(array[i] + " ");
                 }
-                
             }
         }
     }

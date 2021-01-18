b3bf209ef664a91977ba0870675ac97f688f614f
seydametov <eldar.seyametov@mail.ru>
2021-01-18 16:41:20 +0300

    Task 2. Thread

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Threading;
 
 namespace Laboratories320
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             Console.WriteLine("Leave hope behind all who enters here...");
             Console.WriteLine();
+           
         }
     }
 }

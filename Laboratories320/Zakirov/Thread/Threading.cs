82d55830f4b3256dfd1c4854ad8448975b8ca93a
Ranis666 <zakirov.ranis@gmail.com>
2021-01-18 20:55:57 +0300

    Not finished - 2 practice

--- Laboratories320/Zakirov/Thread/Threading.cs
+++ Laboratories320/Zakirov/Thread/Threading.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Laboratories320.Zakirov.Thread
 {
     class Threading
     {
-        public int[] number;
+        public static int[] numbers = new int[7] { 3, 5, 1, 7, 2, 2, 9};
+
+        public static void Sum()
+        {
+            int sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine("Сумма всех чисел: " + sum);
+        }
+
+
+        
     }
 }

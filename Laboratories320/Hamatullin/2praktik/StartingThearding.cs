801b18536c69cb268d493ac0d451b7239a4856de
Hamich <ihamatullin@gmail.com>
2021-01-18 09:11:16 +0300

    + class "Thearding" with  3 methods

---
+++ Laboratories320/Hamatullin/2praktik/StartingThearding.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Hamatullin._2praktik
+{
+    class StartingThearding
+    {
+        
+    }
+}
--- Laboratories320/Hamatullin/2praktik/Thearding.cs
+++ Laboratories320/Hamatullin/2praktik/Thearding.cs
@@ -6,5 +6,49 @@ namespace Laboratories320.Hamatullin._2praktik
 {
     class Thearding
     {
+        private int[] array;
+        public int[] Array { get; set; }
+        public Thearding(int[] Array)
+        {
+            this.Array = Array;
+        }
+        public void Summa()
+        {
+            int sum = 0;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                sum += Array[i];
+            }
+            Console.WriteLine($"Сумма чисел в массиве: {sum}");
+        }
+        public void MaxElem()
+        {
+            int max = 0;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if(max < Array[i])
+                {
+                    max = Array[i];
+                }
+            }
+            Console.WriteLine($"Максимальный элемент в массиве: {max}");
+        }
+        public void SortArray()
+        {
+            int a = 0;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                for (int j = i + 1; j < Array.Length; j++)
+                {
+                    if (Array[i] > Array[j])
+                    {
+                        a = Array[i];
+                        Array[i] = Array[j];
+                        Array[j] = a;
+
+                    }
+                }
+            }
+        }
     }
 }

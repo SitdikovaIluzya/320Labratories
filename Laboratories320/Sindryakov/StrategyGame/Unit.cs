db0ef82df96698af35ec10b6f9ed13a7745779e2
Sindryakov <clashervclashe@gmail.com>
2021-01-18 08:52:17 +0300

    Commit

---
+++ Laboratories320/Sindryakov/StrategyGame/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Sindryakov.StrategyGame
+{
+    abstract class Unit
+    {
+        public string Name { get; set; }
+        public double Healt { get; set; }
+        public Unit(string name, double healt)
+        {
+
+        }
+    }
+}
---
+++ Laboratories320/Sindryakov/StrategyGame/WorkerUnit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Sindryakov.StrategyGame
+{
+    class WorkerUnit
+    {
+    }
+}

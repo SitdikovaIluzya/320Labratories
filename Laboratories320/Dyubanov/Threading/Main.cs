de2a2d98607dc5a65e5408b2e5bc06eaab121fdc
1exsize1 <vlad.dyubanov@gmail.com>
2021-01-18 10:13:48 +0300

    finish "Threading"

---
+++ Laboratories320/Dyubanov/Threading/Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Laboratories320.Dyubanov.Threading
+{
+    class Main
+    {
+        public void Thread1()
+        {
+            int[] array = new int[10] { 4, 56, 34, 5, 6, 5, 3, 453, 6, 32 };
+            Thread thread1 = new Thread(sumOfElement);
+            Thread thread2 = new Thread(maxElement);
+            Thread thread3 = new Thread(sort);
+            thread1.Start(array);
+            thread2.Start(array);
+            thread3.Start(array);
+            array[0] = 1;
+        }
+
+        private void sumOfElement(object obj)
+        {
+            double sum = 0;
+            int[] arr = (int[])obj;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = arr[i] + sum;
+            }
+            Console.WriteLine("Sum = " + sum);
+        }
+
+        private void maxElement(object obj)
+        {
+            int[] arr = (int[])obj;
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; ++i)
+                if (arr[i] > max) max = arr[i];
+            Console.WriteLine("Max = " + max);
+        }
+
+        private void sort(object obj)
+        {
+            Array.Sort((int[])obj);
+            int[] arr = (int[])obj;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(i + " arr element = " + arr[i]);
+            }
+        }
+    }
+}
--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Laboratories320.Dyubanov.StrategyGame;
+using Laboratories320.Dyubanov.Threading;
 using System;
+using System.Threading;
 
 namespace Laboratories320
 {
@@ -7,15 +9,7 @@ namespace Laboratories320
     {
         static void Main(string[] args)
         {
-            Attacker att1 = new Attacker("att1", 50, 100);
-            Attacker att2 = new Attacker("att2", 5, 80);
-            Archer arc1 = new Archer("arc1", 30, 50);
-            Citadel cit1 = new Citadel("cit1", 30, 50);
-            Defender def1 = new Defender("def1", 50, 80);
 
-
-            att1.SendMessage();
-            def1.DamageEnemy(att2);
         }
     }
 }

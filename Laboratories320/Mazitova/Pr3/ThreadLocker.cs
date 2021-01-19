10c2a8f533fdb918c80721c94ba41451613caba8
Mazitova <77325891+Mazitova@users.noreply.github.com>
2021-01-19 15:09:13 +0300

    changed

---
+++ Laboratories320/Mazitova/Pr3/ThreadLocker.cs
@@ -0,0 +1,20 @@
+﻿using ImTools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Laboratories320.Mazitova.Pr3
+{
+    class ThreadLocker
+    {
+        static int[] array1 = new int[10];
+        static int[] array2 = new int[10];
+        static int[] array3 = new int[10];
+        
+        for (int i = 0; i<2; I++)
+			{
+
+			}
+    }
+}

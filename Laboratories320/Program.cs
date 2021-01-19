e3a172af64bd77b6577a9848aa063f2ca5eee5b5
ilinasaetova <saetova_i@list.ru>
2021-01-19 15:08:56 +0300

    end lesson 19.01.21

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -5,47 +5,27 @@ namespace Laboratories320
 {
     class Program
     {
-        static int[] array = { 1, 2, 3, 4, 6, 7, 8, 9, 0 };
+        static int[] array_1 = new int[10];
+        static int[] array_2 = new int[10];
+        static int[] array_3 = new int[10];
+        static object locker = new object();
         static void Main(string[] args)
         {
-            Thread t1 = new Thread(Max);
-            Thread t2 = new Thread(Sum);
-            Thread t3 = new Thread(Sort);
-            t1.Start();
-            t2.Start();
-            t3.Start();
+
         }
 
-        static void Max()
+        static void Random_Ar()
         {
-            int max = 0;
-            for (int i = 1; i < array.Length; i++)
+            lock (locker)
             {
-                if (array[i] > array[i - 1])
+                Random rnd = new Random();
+                for(int i = 0; i < 10; i++)
                 {
-                    max = array[i];
+                    array_1[i] = rnd.Next();
+                    array_2[i] = rnd.Next();
                 }
             }
-            Console.WriteLine("Максимум " + max);
-            Thread.Sleep(500);
         }
 
-        static void Sum()
-        {
-            double sum = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                sum = sum + array[i];
-            }
-            Console.WriteLine("Сумма " + sum);
-            Thread.Sleep(500);
-        }
-
-        static void Sort()
-        {
-            Array.Sort(array);
-            Console.WriteLine(String.Join(" ", array));
-            Thread.Sleep(500);
-        }
     }
 }

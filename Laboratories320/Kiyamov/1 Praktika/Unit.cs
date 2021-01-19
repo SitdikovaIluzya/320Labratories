a0b83cd904f6c14d89c83ebeec11f570331c394c
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-20 00:24:52 +0300

    Create Unit.cs

---
+++ Laboratories320/Kiyamov/1 Praktika/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Kiyamov._1_Praktika
+{
+    class Unit
+    {
+        public int Hp { get; set; }
+        public int Name { get; set; }
+        public static void Destruction(Unit unit)
+        {
+            unit.Hp = 0;
+            Console.WriteLine(unit.Name + " уничтожен");
+        }
+
+    }
+}

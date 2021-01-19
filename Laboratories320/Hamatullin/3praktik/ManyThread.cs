496e581570ea449cf70e41673bb0b5b2d3da2d16
Hamich <ihamatullin@gmail.com>
2021-01-19 14:31:45 +0300

    start lesson (19.01.2021)

---
+++ Laboratories320/Hamatullin/3praktik/ManyThread.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Laboratories320.Hamatullin._3praktik
+{
+    class ManyThread
+    {
+        int[] mass1 = new int[10];
+        int[] mass2 = new int[10];
+        int[] result = new int[10];
+        
+    }
+}

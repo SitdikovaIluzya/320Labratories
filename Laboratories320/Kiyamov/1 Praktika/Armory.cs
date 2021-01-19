136e6ac8173db0564598c07d9364f454e09e4ba5
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-20 00:26:14 +0300

    Create Armory.cs

---
+++ Laboratories320/Kiyamov/1 Praktika/Armory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Kiyamov._1_Praktika
+{
+    class Armory 
+    {
+        public Armory(string Name, int Hp, int Width, int Length) : base(Name, Hp, Width, Length)
+    }
+}

98bc25243b7d86a3c48b72c153d6c011aa290860
Valieva <guz.valieva122@gmail.com>
2021-01-19 15:01:15 +0300

    start lab3 commit

---
+++ Laboratories320/Lab3/Threading2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Lab3
+{
+    class Threading2
+    {
+        int[] array1;
+        int[] array2;
+        public int[] Array1 { get => array1; set => array1 = value; }
+        public int[] Array2 { get => array2; set => array2 = value; }
+
+        public Threading2(int[] array1, int[] array2)
+        {
+            this.Array1 = array1;
+            this.Array2 = array1;
+        }
+
+        public void Generator()
+        {
+            Random rnd = new Random;
+
+        }
+    }
+}

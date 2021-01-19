d03d7ded3816ad42693419447970186d69eabd58
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-19 14:34:31 +0300

    Create ThreadLock.cs

---
+++ Laboratories320/Kiyamov/ThreadLocker/ThreadLock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Kiyamov.ThreadLocker
+{
+    class ThreadLock
+    {
+        int[] Mass1 = new int [3];
+        int[] Mass2 = new int [5];
+        int[] Mass3 = new int [2];
+
+
+    }
+
+
+}

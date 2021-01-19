ff9921ac572ade06958d91813f21ad4ac15848ec
Sindryakov <clashervclashe@gmail.com>
2021-01-19 14:49:45 +0300

    Start lesson

---
+++ Laboratories320/Sindryakov/ThreadLocker/Locker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Sindryakov.ThreadLocker
+{
+    class Locker
+    {
+        // начало выполнения 3 практики
+    }
+}

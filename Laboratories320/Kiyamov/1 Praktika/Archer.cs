e69f6729c078801c4ec53b13c9cb9233f2a03104
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-20 00:23:56 +0300

    Update Archer.cs

--- Laboratories320/Kiyamov/1 Praktika/Archer.cs
+++ Laboratories320/Kiyamov/1 Praktika/Archer.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace Laboratories320.Kiyamov._1_Praktika
 {
-    class Archer
+    class Archer : IToDamageble
     {
+        public int Damage { get; set; }
+        public Archer(string Name, int hp,int Damage) : base(Name, hp)
+        {
+            this.Damage = Damage;
+        }
     }
 }

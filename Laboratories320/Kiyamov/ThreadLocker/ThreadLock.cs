5e7a4659971f946fd17abcb8a786070635c98069
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-19 15:04:49 +0300

    Update ThreadLock.cs

--- Laboratories320/Kiyamov/ThreadLocker/ThreadLock.cs
+++ Laboratories320/Kiyamov/ThreadLocker/ThreadLock.cs
@@ -1,17 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using Laboratories320.Kiyamov.ThreadLocker;
 
 namespace Laboratories320.Kiyamov.ThreadLocker
 {
     class ThreadLock
     {
-        int[] Mass1 = new int [3];
-        int[] Mass2 = new int [5];
-        int[] Mass3 = new int [2];
+        int[] array = new int[5];
+        static int[] Mass1 = new int[5];
+        static int[] Mass2 = new int[5];
+        static int[] resuilts = new int[5];
 
+
+        
+        
+       
+        public void MassiveGenarate()
+        {
+      
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 20);
+            }
+            Console.WriteLine("Massive : " + Mass1);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item + " ");
+                
+            }
+      
+        }
 
     }
 
-
 }
+
+
+    
+
+

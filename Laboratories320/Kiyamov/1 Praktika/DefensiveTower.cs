29d17ac869c213486ec40c1057785a7dbfad1df8
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-20 00:24:15 +0300

    Create DefensiveTower.cs

---
+++ Laboratories320/Kiyamov/1 Praktika/DefensiveTower.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Kiyamov._1_Praktika
+{
+    class DefensiveTower : UnitBuilding,IToDamageble
+    {
+        public int Damage { get; set; }
+        public DefensiveTower(string Name, int Hp, int Width,int Damage) : base(Name, Hp, Width)
+        {
+            this.Damage = Damage;
+        }
+    }
+}

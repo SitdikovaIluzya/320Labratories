8ab5132aaf32f0d9e105eecb1e1e71662f753254
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-20 00:24:39 +0300

    Create IToDamageble.cs

---
+++ Laboratories320/Kiyamov/1 Praktika/IToDamageble.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Kiyamov._1_Praktika
+{
+    interface IToDamageble
+    {
+          
+        int Damage { get; set; }
+    }
+}
+

c228706ef33b0dfc89e2f20ee4b7c0294d69171c
goosberry <saetova_i@list.ru>
2021-01-19 15:06:14 +0300

    finish tast 18.01.21

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -5,15 +5,50 @@ namespace Laboratories320
 {
     class Program
     {
+        static double[] arr = { 1, 8, 3, 9, 5 };  
+        static double sum = 0;
+        static double max;
         static void Main(string[] args)
         {
-            Console.WriteLine("Leave hope behind all who enters here...");
-            Console.WriteLine('l');
 
-            //Thread t = new Thread(thread.ThreadSum.Sum);
+            Thread t = new Thread(Sum);
+            t.Start(); 
+            Thread t1 = new Thread(Max);
+            t1.Start();
+            Thread t2 = new Thread(Sort);
+            t2.Start();
+            
+        }
+        static void Sum()
+        {
+            
+         
+            for (int i = 0; i < arr.Length; i++)
+            {
 
-            //t.Start();
-
+                sum = sum + arr[i];
+            }
+            Console.WriteLine("Сумма= "+sum);
+        }
+       static void Max()
+        {
+            
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[i - 1])
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine("Максимальное число= "+max);
+       }
+        static void Sort()
+        {
+            Array.Sort(arr);
+            for (int i =0; i < arr.Length; i++)
+            {
+                 Console.Write(arr[i]+" " );
+            }
            
         }
     }

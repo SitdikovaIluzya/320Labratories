beeaa7606c7e09c15bcb63652fd3178fe9c65bae
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-20 00:24:22 +0300

    Create IFix.cs

---
+++ Laboratories320/Kiyamov/1 Praktika/IFix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Kiyamov._1_Praktika
+{
+    interface IFix
+    {
+            
+        void RepairBuild(UnitBuilding unitBuilding);
+    }
+}
+

50ab077e8ffb0a0acc685f01d921214dc5467f54
keston616 <airat.shigapov2015@yandex.ru>
2021-01-19 15:08:12 +0300

    EndLessen(19.01.2021)

---
+++ Laboratories320/Shigapov/Task3/ThreadLocker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Laboratories320.Shigapov.Task3
+{
+    class ThreadLocker
+    {
+        public static int[] array1 = new int[10];
+        public static int[] array2 = new int[10];
+        public static int[] array3 = new int[10];
+        static object locker = new object();
+
+        public void BaseThread()
+        {
+            Thread thread1 = new Thread(new ParameterizedThreadStart(Rnd));
+            Thread thread2 = new Thread(new ThreadStart());
+        } 
+
+        public static void Sum()
+        {
+
+                for (int i = 0; i < 0; i++)
+                {
+                    array3[i] = array1[i]+array2[i];
+                }
+        }
+
+
+        public void Rnd(object obj)
+        {
+            lock (locker)
+            {
+                int[] array = (int[])obj;
+                Random rnd = new Random();
+                for (int i = 0; i < 0; i++)
+                {
+                    array[i] = rnd.Next(-10, 10);
+                }
+
+            }
+        }
+
+    }
+}

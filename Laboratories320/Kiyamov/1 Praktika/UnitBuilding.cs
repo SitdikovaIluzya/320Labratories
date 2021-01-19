98ecb40351cabfcd361495efecc0f17e6bddbe8c
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-20 00:24:58 +0300

    Create UnitBuilding.cs

---
+++ Laboratories320/Kiyamov/1 Praktika/UnitBuilding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Kiyamov._1_Praktika
+{
+    class UnitBuilding : Unit
+    {
+        public UnitBuilding(string Name, int Hp) : base(Name, Hp)
+        {
+            
+            this.Hp = Hp;
+
+           
+        }
+
+    }
+}

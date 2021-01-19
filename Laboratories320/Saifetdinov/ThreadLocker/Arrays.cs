45b434df091e769a1e30360cd2cafb89fe6eb327
Эмиль <77325950+ImilSaifetdinov@users.noreply.github.com>
2021-01-19 15:06:49 +0300

    End lesson

--- Laboratories320/Saifetdinov/ThreadLocker/Arrays.cs
+++ Laboratories320/Saifetdinov/ThreadLocker/Arrays.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Laboratories320.ThreadLocker
 {
     class Arrays
     {
-        static int[] array1 = new int[10];
+        public static void Thread()
+        {
+            Thread thread1 = new Thread(Gen);
+        }
+        public static void Gen()
+        {
+            int[] array = new int[9];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-100, 101);
+                Console.Write(array[i] + " ");
+            }
+        }
     }
 }

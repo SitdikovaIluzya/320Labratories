b63a09cb64009456af669be27e0abd0fc467966a
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-20 00:24:07 +0300

    Create Builder.cs

---
+++ Laboratories320/Kiyamov/1 Praktika/Builder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Kiyamov._1_Praktika
+{
+    class Builder : IFix
+
+    { 
+        public void RepairBuild(UnitBuilding unitBuilding)
+        {
+            unitBuilding.Hp += 20;
+            Console.WriteLine("Fixed 20 HP");
+        }
+    }
+}

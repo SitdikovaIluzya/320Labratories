b8a75848e8bc339772bdc5a9d53f81852d1dd0ab
Marsel011 <77325786+Marsel011@users.noreply.github.com>
2021-01-19 21:18:28 +0300

    Finish ThreadLocker

--- Laboratories320/Musin/ThreadLocker/Locker.cs
+++ Laboratories320/Musin/ThreadLocker/Locker.cs
@@ -16,31 +16,38 @@ namespace Laboratories320.Musin.ThreadLocker
         {
             Thread thread1 = new Thread(Generate);
             Thread thread2 = new Thread(Summ);
-            lock(locker)
-            {
-                thread1.Start();
-            }
+            thread1.Start();
             thread2.Start();
         }
         public static void Generate()
         {
-            Random rnd = new Random();
-            for(int i = 0; i < array1.Length; i++)
+            lock (locker)
             {
-                array1[i] = rnd.Next(10);
-            }
+                Random rnd = new Random();
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    array1[i] = rnd.Next(10);
+                    Console.Write(array1[i] + " ");
+                }
 
-            for (int i = 0; i < array2.Length; i++)
-            {
-                array2[i] = rnd.Next(10);
+                Console.WriteLine();
+
+                for (int i = 0; i < array2.Length; i++)
+                {
+                    array2[i] = rnd.Next(10);
+                    Console.Write(array1[i] + " ");
+                }
+                Console.WriteLine();
             }
         }
         
         public static void Summ()
         {
+            Thread.Sleep(100);
             for (int i = 0; i < result.Length; i++)
             {
                 result[i] = array1[i] + array2[i];
+                Console.Write(result[i] + " ");
             }
         }
 

ab61b3317991fa9eb5bbc2cfd4eac63cb470902a
Marsel011 <77325786+Marsel011@users.noreply.github.com>
2021-01-19 14:42:14 +0300

    Start lesson 19.01.2021

---
+++ Laboratories320/Musin/ThreadLocker/Locker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Musin.ThreadLocker
+{
+    class Locker
+    {
+        static int[] array1 = new int[10];
+        static int[] array2 = new int[10];
+        static int[] result = new int[10];
+
+    }
+}

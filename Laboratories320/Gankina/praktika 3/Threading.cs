8d41d504f8b409db5357ef7276e5812f7c119da8
Gankina <mari.gankina@bk.ru>
2021-01-20 18:58:03 +0300

    Home start 20.01

--- Laboratories320/Gankina/praktika 3/Threading.cs
+++ Laboratories320/Gankina/praktika 3/Threading.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Laboratories320.Gankina.praktika_3
 {
@@ -11,7 +12,7 @@ namespace Laboratories320.Gankina.praktika_3
         public int[] Array1 { get => array1; set => array1 = value; }
         public int[] Array2 { get => array2; set => array2 = value; }
 
-        public Threading2(int[] array1, int[] array2)
+        public Threading(int[] array1, int[] array2)
         {
             this.Array1 = array1;
             this.Array2 = array1;

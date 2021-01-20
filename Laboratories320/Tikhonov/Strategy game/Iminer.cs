5ebea24fc0f5000beb95e2e6056bdc9c07e78342
Tracerzod <codateh@gmail.com>
2021-01-20 09:30:01 +0300

    Completion of a "Strategy game" practice

---
+++ Laboratories320/Tikhonov/Strategy game/Iminer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Tikhonov.Strategy_game
+{
+    interface Iminer : IbaseUnit
+    {
+        int Speed { get; set; }
+        void mine();
+        void move();
+    }
+}
---
+++ Laboratories320/Tikhonov/Strategy game/Ipriest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Tikhonov.Strategy_game
+{
+    interface Ipriest : IbaseUnit
+    {
+        int Speed { get; set; }
+        int Damage { get; set; }
+        void heal_ally(IbaseUnit unit);
+        void move();
+
+    }
+}

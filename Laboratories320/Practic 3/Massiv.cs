f3bd0d62212e34661e811b811089cd1d84fe2af9
@GGRustem <gaifullin.rustem2018@gmail.com>
2021-01-20 22:20:20 +0300

    Gaifullin
    End Practic3

--- Laboratories320/Practic 3/Massiv.cs
+++ Laboratories320/Practic 3/Massiv.cs
@@ -1,31 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Laboratories320.Practic_3
 {
     class Massiv
     {
-       
-            public static int[] array1 = new int[10];
-            public static int[] array2 = new int[10];
-            public static int[] array3 = new int[10];
 
-            static object Sum = new object();
-            static object Result = new object();
+        public static int[] array1 = new int[10];
+        public static int[] array2 = new int[10];
+        public static int[] array3 = new int[10];
 
-            public static void BaseThread()
+        static object Sum = new object();
+        static object Result = new object();
+
+        public static void List()
+        {
+            Thread thread1 = new Thread(new ThreadStart(Rnd));
+            Thread thread2 = new Thread(new ThreadStart(Summ));
+            thread1.Start();
+            Thread.Sleep(10);
+            thread2.Start();
+            Console.WriteLine("[{0}]", string.Join(", ", array1));
+            Console.WriteLine("[{0}]", string.Join(", ", array2));
+            Console.WriteLine("[{0}]", string.Join(", ", array3));
+        }
+        public static void Rnd()
+        {
+            lock (Sum)
             {
-                Massiv thread1 = new Massiv(new ThreadStart(Rnd));
-                Massiv thread2 = new Massiv(new ThreadStart(Summ));
-                thread1.Start();
-                Thread.Sleep(10);
-                thread2.Start();
-                Console.WriteLine("[{0}]", string.Join(", ", array1));
-                Console.WriteLine("[{0}]", string.Join(", ", array2));
-                Console.WriteLine("[{0}]", string.Join(", ", array3));
-            }
+                Random rnd = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    array1[i] = rnd.Next(-10, 10);
+                    array2[i] = rnd.Next(-10, 10);
+                }
+                Console.WriteLine("Generating");
 
+            }
+        }
         public static void Summ()
         {
             lock (Result)
--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -12,7 +12,6 @@ namespace Laboratories320
 
             Massiv.List();
             
-            Massiv.();
 
 
         }

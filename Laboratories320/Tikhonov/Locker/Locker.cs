21370276cab82868a2db82f2e73c23e70d2f6666
Tracerzod <codateh@gmail.com>
2021-01-20 08:26:43 +0300

    Completion of a "ThreadLock" practice

--- Laboratories320/Tikhonov/Locker/Locker.cs
+++ Laboratories320/Tikhonov/Locker/Locker.cs
@@ -7,33 +7,48 @@ namespace Laboratories320.Tikhonov.Locker
 {
     class Thread_locking
     {
+        static Object locker1 = new Object();
+
+
         private static int[] FirstArray = new int[10];
         private static int[] SecondArray = new int[10];
         private static int[] FinalArray = new int[10];
         public static void Threads()
         {
             ArrObj Arrays = new ArrObj(FirstArray, SecondArray);
-            Thread threadSum = new Thread(sumOfElements);
             Thread threadGen = new Thread(arrGen);
-            threadSum.Start(Arrays);
+            Thread threadSum = new Thread(sumOfElements);
             threadGen.Start(Arrays);
-
+            Thread.Sleep(100);
+            threadSum.Start(Arrays);
         }
         private static void sumOfElements(Object obj)
         {
-            
+            lock (locker1)
+            {
+                for (int i = 0; i < FirstArray.Length; i++)
+                {
+                    FinalArray[i] = FirstArray[i] + SecondArray[i];
+                }
+                Console.WriteLine("[{0}]", string.Join(", ", FinalArray));
+            }
         }
 
-      
         private static void arrGen(object obj)
         {
             Random rand = new Random();
-            for (int i = 0; i < FirstArray.Length; i++)
+            lock(locker1)
             {
-                FirstArray[i] = rand.Next(10);
-                SecondArray[i] = rand.Next(10);
-            }
+                for (int i = 0; i < FirstArray.Length; i++)
+                {
+                    FirstArray[i] = rand.Next(10);
+                    SecondArray[i] = rand.Next(10);
+                }
+                Console.WriteLine("[{0}]", string.Join(", ", FirstArray));
+                Console.WriteLine("[{0}]", string.Join(", ", SecondArray));
 
-        }
+
+                }
+            }
     }
 }

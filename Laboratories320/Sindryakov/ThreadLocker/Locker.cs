8ab214f287577dd67f5986bfcb112ca0f667fde2
Sindryakov <77325774+Sindryakov@users.noreply.github.com>
2021-01-20 20:08:42 +0300

    Commit at home 2

--- Laboratories320/Sindryakov/ThreadLocker/Locker.cs
+++ Laboratories320/Sindryakov/ThreadLocker/Locker.cs
@@ -7,23 +7,16 @@ namespace Laboratories320.Sindryakov.ThreadLocker
 {
     class Locker
     {
-            // начало выполнения 3 практики
-            //int[] numbers1 = { 3, 4, 45, 29, 2, 23, 48, 33, 1, 54 };
-            //int[] numbers2 = { 8, 4, 13, 16, 2, 15, 48, 33, 5, 52 };
-            //int[] numbers3 = { 8, 4, 13, 16, 2, 15, 48, 33, 5, 43 };
+        //3 практика
+        static int[] outcome = new int[10];
+        static int[] numbers1 = new int[10];
+        static int[] numbers2 = new int[10];
 
-            static int[] outcome = new int[10];
-            static int[] numbers1 = new int[10];
-            static int[] numbers2 = new int[10];
+        static object locker = new object();
 
-            static object locker = new object();
-
-        //Thread thr1 = new Thread(Summ);
-        //Thread thr2 = new Thread(Generate);
 
         //lock - написать
 
-        //thr2.Start(numbers2);
         public static void GoOff()
         {
             Thread thr1 = new Thread(Transform);
@@ -31,28 +24,20 @@ namespace Laboratories320.Sindryakov.ThreadLocker
             thr1.Start(numbers1);
             thr2.Start(numbers2);
         }
-        public static void TotalSum(object obj)
+        public static void TotalSum()
         {
-
-            int[] nums1 = (int[])obj;
-            int sum1 = 0;
-            foreach (int value in nums1)
+            Thread.Sleep(100);
+            for (int i = 0; i < outcome.Length; i++)
             {
-                sum1 += value;
             }
-
-            int[] nums2 = (int[])obj;
-            int sum2 = 0;
-            foreach (int value in nums2)
-            {
-                sum2 += value;
-            }
-            int sum = sum1 + sum2;
             Console.WriteLine("Сумма элементов массива:" + sum);
         }
         public static void Transform(object obj)
         {
-            Console.WriteLine("Генерация: ");
+            lock (locker)
+            {
+                Console.WriteLine("Генерирование значений - ");
+            }
         }
     }
 }

03f844e97781b98cfa41f499bdd70a73c69e1b8a
railgim <gimatdinov.2002.2018@gmail.com>
2021-01-20 22:33:43 +0300

    Update ThreadLocker.cs

--- Laboratories320/Gimatdinov/Task3/ThreadLocker.cs
+++ Laboratories320/Gimatdinov/Task3/ThreadLocker.cs
@@ -10,35 +10,46 @@ namespace Laboratories320.Shigapov.Task3
         public static int[] array1 = new int[10];
         public static int[] array2 = new int[10];
         public static int[] array3 = new int[10];
-        static object locker = new object();
+
+        static object SumLocker = new object();
+        static object ResultLocker = new object();
 
         public void BaseThread()
         {
-            Thread thread1 = new Thread(new ParameterizedThreadStart(Rnd));
-            Thread thread2 = new Thread(new ThreadStart());
-        } 
+            Thread thread1 = new Thread(new ThreadStart(Rnd));
+            Thread thread2 = new Thread(new ThreadStart(Sum));
+            thread1.Start();
+            Thread.Sleep(10);
+            thread2.Start();
+            Console.WriteLine("[{0}]", string.Join(", ", array1));
+            Console.WriteLine("[{0}]", string.Join(", ", array2));
+            Console.WriteLine("[{0}]", string.Join(", ", array3));
+        }
 
         public static void Sum()
         {
-
-                for (int i = 0; i < 0; i++)
+            lock (ResultLocker)
+            {
+                for (int i = 0; i < 10; i++)
                 {
-                    array3[i] = array1[i]+array2[i];
+                    array3[i] = array1[i] + array2[i];
                 }
+                Console.WriteLine("Sum");
+            }
         }
 
 
-        public void Rnd(object obj)
+        public static void Rnd()
         {
-            lock (locker)
+            lock (SumLocker)
             {
-                int[] array = (int[])obj;
                 Random rnd = new Random();
-                for (int i = 0; i < 0; i++)
+                for (int i = 0; i < 10; i++)
                 {
-                    array[i] = rnd.Next(-10, 10);
+                    array1[i] = rnd.Next(-10, 10);
+                    array2[i] = rnd.Next(-10, 10);
                 }
-
+                Console.WriteLine("Generating");
             }
         }
 

36ca5f08af5c41379eedb6aa86a4719f63d10f59
Sindryakov <77325774+Sindryakov@users.noreply.github.com>
2021-01-21 09:23:26 +0300

    End Lesson

--- Laboratories320/Sindryakov/TaskRedone/Tasking.cs
+++ Laboratories320/Sindryakov/TaskRedone/Tasking.cs
@@ -14,7 +14,30 @@ namespace Laboratories320.Sindryakov.TaskRedone
         static int[] numbers1 = new int[10];
         static int[] numbers2 = new int[10];
 
-        
+        public static void Tasks()
+        {
+            Task task1 = new Task(() => Transform());
+
+            Task task2 = new Task(() => TotalSum());
+
+            task1.Start();
+            task2.Start();
+            task2.Wait();
+        }
+
+
+        public static void TotalSum() //сумма
+        {
+            for (int i = 0; i < outcome.Length; i++)
+            {
+                outcome[i] = numbers1[i] + numbers2[i];
+                Console.WriteLine("Сумма элементов массива: ");
+                Console.Write(outcome[i] + " ");
+                Console.WriteLine();
+            }
+
+        }
+
         public static void Transform() //генерируем
         {
 

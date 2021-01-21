806f87b31e554295dfcaf988cb35a8a8e8e609cb
Ranis666 <zakirov.ranis@gmail.com>
2021-01-21 08:50:27 +0300

    Start lesson 21.01.21

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Laboratories320.Zakirov._2Thread;
 using Laboratories320.Zakirov._3ThreadLocker;
+using System.Threading.Tasks;
 
 namespace Laboratories320
 {
@@ -8,7 +9,11 @@ namespace Laboratories320
     {
         static void Main(string[] args)
         {
-            ThreadLock.StartingThread();
+            Task task1 = new Task(() => Console.WriteLine("*"));
+            task1.Start();
+            Task task2 = Task.Run(() => Console.WriteLine("#"));
+            task1.Wait();
+            task2.Wait();
         }
     }
 }

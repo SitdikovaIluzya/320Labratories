4daecd243aa13186a925bc96d06b1c16a730800d
Marsel011 <77325786+Marsel011@users.noreply.github.com>
2021-01-21 13:16:02 +0300

    End Lesson 21.01.2021

--- Laboratories320/Musin/ContinuationTasks/ContinTasks.cs
+++ Laboratories320/Musin/ContinuationTasks/ContinTasks.cs
@@ -6,6 +6,26 @@ namespace Laboratories320.Musin.ContinuationTasks
 {
     class ContinTasks
     {
+        static int[] array = new int[20];
+        static int result = 1;
+        public static void Generator()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 5);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
 
+        public static void Multiplier()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                result *= array[i];
+            }
+            Console.WriteLine(result);
+        }
     }
 }

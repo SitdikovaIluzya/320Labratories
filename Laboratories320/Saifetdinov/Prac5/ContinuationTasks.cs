f5f7206bb10d7bf999eef8697ba2abc366143c85
Эмиль <77325950+ImilSaifetdinov@users.noreply.github.com>
2021-01-21 12:38:29 +0300

    start lesson prac5

---
+++ Laboratories320/Saifetdinov/Prac5/ContinuationTasks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Saifetdinov.Prac5
+{
+    static class ContinuationTasks
+    {
+
+        static int[] array = new int[20];
+        public static void GenerationElementsArray()
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-10, 10);
+            }
+        }
+        public static void Multication()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= array[i];
+            }
+        }
+    }
+}

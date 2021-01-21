9fcc70be544ecdad4768c30a13524862eb21c8a9
Sindryakov <77325774+Sindryakov@users.noreply.github.com>
2021-01-21 13:11:16 +0300

    End Lesson

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-namespace Laboratories320
+namespace Laboratories320.Sindryakov.ContinuationTasks
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Tasks.Task();
             Console.WriteLine("");
             Console.WriteLine();
+            
         }
     }
 }
--- Laboratories320/Sindryakov/ContinuationTasks/Tasks.cs
+++ Laboratories320/Sindryakov/ContinuationTasks/Tasks.cs
@@ -1,12 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Laboratories320.Sindryakov.ContinuationTasks
 {
     class Tasks
     {
-        //int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 60,25,34,56,78,89,14,27,18,16,29 };
-        static int[] numbers1 = new int[20];
+
+        static int[] numbers = new int[20];
+        //static int[] outcome = new int[10];
+        static int a = 5;
+        public static void Task()
+        {
+            Task task1 = new Task(() => TotalСomposition());
+            //Task task1 = new Task(() => ParityCheck());
+            task1.Start();
+
+
+        }
+
+        public static void TotalСomposition() //произведение
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                a *= numbers[i];
+                Console.WriteLine("Произведение элементов массива: ");
+                Console.Write(numbers[i] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        // деление на 10 через if
+        public static void ParityCheck()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 10 == 0)
+                {
+
+                    //a++;
+                }
+            }
+        }
+
+
+        public static void Transform() //генерируем
+        {
+
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(10, 20);
+                Console.WriteLine("Генерирование значений - ");
+                Console.Write(numbers[i] + " ");
+            }
+        }
     }
 }

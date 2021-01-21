2228831f0dc3635abad1ac7c312a24844aba57f3
Sindryakov <77325774+Sindryakov@users.noreply.github.com>
2021-01-21 09:19:35 +0300

    Commit at lesson

--- Laboratories320/Sindryakov/TaskRedone/Tasking.cs
+++ Laboratories320/Sindryakov/TaskRedone/Tasking.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Laboratories320.Sindryakov.TaskRedone
 {
@@ -9,55 +10,32 @@ namespace Laboratories320.Sindryakov.TaskRedone
         //4 практика
         //переделываем 3 практику 
 
+        static int[] outcome = new int[10];
+        static int[] numbers1 = new int[10];
+        static int[] numbers2 = new int[10];
 
+        
+        public static void Transform() //генерируем
+        {
 
-        //static int[] outcome = new int[10];
-        //static int[] numbers1 = new int[10];
-        //static int[] numbers2 = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < numbers1.Length; i++)
+            {
+                numbers1[i] = rnd.Next(1, 9);
+                Console.WriteLine("Генерирование значений - ");
+                Console.Write(numbers1[i] + " ");
+                Console.WriteLine();
+            }
 
-        //static object locker = new object();
-
-
-        //public static void GoOff() //старт
-        //{
-        //    Thread thr1 = new Thread(Transform);
-        //    Thread thr2 = new Thread(TotalSum);
-        //    thr1.Start(numbers1);
-        //    thr2.Start(numbers2);
-        //}
-        //public static void TotalSum() //сумма
-        //{
-        //    Thread.Sleep(100);
-        //    for (int i = 0; i < outcome.Length; i++)
-        //    {
-        //        outcome[i] = numbers1[i] + numbers2[i];
-        //        Console.WriteLine("Сумма элементов массива: ");
-        //        Console.Write(outcome[i] + " ");
-        //        Console.WriteLine();
-        //    }
-
-        //}
-        //public static void Transform() //генерируем
-        //{
-        //    lock (locker)
-        //    {
-        //        Random rnd = new Random();
-        //        for (int i = 0; i < numbers1.Length; i++)
-        //        {
-        //            numbers1[i] = rnd.Next(1, 9);
-        //            Console.WriteLine("Генерирование значений - ");
-        //            Console.Write(numbers1[i] + " ");
-        //            Console.WriteLine();
-        //        }
-
-        //        for (int i = 0; i < numbers2.Length; i++)
-        //        {
-        //            numbers2[i] = rnd.Next(1, 9);
-        //            Console.WriteLine("Генерирование значений - ");
-        //            Console.Write(numbers2[i] + " ");
-        //            Console.WriteLine();
-        //        }
-        //    }
-        //}
+            for (int i = 0; i < numbers2.Length; i++)
+            {
+                numbers2[i] = rnd.Next(1, 9);
+                Console.WriteLine("Генерирование значений - ");
+                Console.Write(numbers2[i] + " ");
+                Console.WriteLine();
+            }
+        }
     }
 }
+
+

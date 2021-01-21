31e4ad5e7d48a7d7851fe9aee366daafd58bbd7f
keston616 <airat.shigapov2015@yandex.ru>
2021-01-21 13:02:03 +0300

    EndContinuationTasks(21/01/21)

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -2,6 +2,9 @@
 using Laboratories320.Shigapov.Practic2;
 using Laboratories320.Shigapov.Task3;
 using Laboratories320.Shigapov.Task1;
+using System.Threading;
+using System.Threading.Tasks;
+using Laboratories320.Shigapov.ContinuationTasks;
 
 namespace Laboratories320
 {
@@ -11,6 +14,19 @@ namespace Laboratories320
         {
             Console.WriteLine("Leave hope behind all who enters here....");
             Console.WriteLine();
+            //Continuation
+            int[] array = new int[20];
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                array[i] = rnd.Next(1, 10);
+            }
+            Console.WriteLine("Generating");
+            Console.WriteLine("[{0}]", string.Join(", ", array));
+            Task task1 = Task.Run(() => Continuation.Сomposition(array));
+            Task task2 = task1.ContinueWith(mul => Continuation.Even());
+            task2.Wait();
+
         }
     }
 }

512afd76f2a84b3747bae386bb76dc1ed4c81ce5
Hamich <ihamatullin@gmail.com>
2021-01-21 12:32:17 +0300

    start lesson(21.01.21)

---
+++ Laboratories320/Hamatullin/5praktik/ContinuationTasks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratories320.Hamatullin._5praktik
+{
+    class ContinuationTasks
+    {
+        static int[] mass = new int[20];
+        static int product = 1;
+
+        public void Gen()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next(10, 20);
+            }
+        }
+        public void Product()
+        {
+            
+        }
+    }
+}

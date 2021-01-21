58d9a87af3d744907e6f3036972c2de97b046f39
goosberry <saetova_i@list.ru>
2021-01-21 17:35:08 +0300

    finish task 21.01.21 ContinuationTasks

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -16,28 +16,28 @@ namespace Laboratories320
         {
 
             Task task1 = new Task(() => Random_Ar());
+            task1.Start();
 
             Task task2 = task1.ContinueWith((task1) => Comp());
-            Task task3 = task1.ContinueWith((task2) => EvenNum());
-
-            task1.Start();
             task2.Wait();
-            task3.Wait();
 
+            Task task3 = task2.ContinueWith((task2) => EvenNum());
+            task3.Wait();
         }
+
         static void Random_Ar()
         {
-
             Random rnd = new Random();
+
             for (int i = 0; i < arr.Length; i++)
             {
-                arr[i] = rnd.Next(1, 5);
+                arr[i] = rnd.Next(1, 4);
             }
+
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.Write(arr[i] + " ");
             }
-
         }
 
         static void Comp()
@@ -45,30 +45,33 @@ namespace Laboratories320
             for (int i = 0; i < arr.Length; i++)
             {
                 comp = arr[i] * comp;
-            }  
-            Console.WriteLine();
-            Console.WriteLine("Произведение= " + comp);
-        
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("Произведение: " + comp);
         }
 
         static void EvenNum()
         {
             int comp2;
-            Console.WriteLine();
+            Console.Write("Четные числа: ");
 
-            
-
-                comp2 = comp % 10;
-                if (comp2 % 2 == 0)
+            while (comp > 9)
+            {
+              
+                comp2 = comp % 10;  
+                if ((comp2 % 2 == 0) &&(comp2 != 0))
                 {
-                    Console.Write(comp + " ");
+                    Console.Write(comp2+" ");
                 }
+                
                 comp = comp / 10;
-            
+            }
 
-            
-            
+            if ((comp % 2 == 0) && (comp != 0))
+            {
+                Console.Write(comp + " ");
+            }
         }
 
     }

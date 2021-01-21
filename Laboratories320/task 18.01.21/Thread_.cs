be3efb9d4348e01d0b420c712c9c21d9f5816a13
ilinasaetova <saetova_i@list.ru>
2021-01-21 08:57:10 +0300

    Update design

---
+++ Laboratories320/task 18.01.21/Thread_.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Laboratories320.task_18._01._21
+{
+    class Thread_
+    {
+        static int[] array = { 1, 2, 3, 4, 6, 7, 8, 9, 0 };
+        static void Main()
+        {
+            Thread t1 = new Thread(Max);
+            Thread t2 = new Thread(Sum);
+            Thread t3 = new Thread(Sort);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+
+        static void Max()
+        {
+            int max = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > array[i - 1])
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine("Максимум " + max);
+            Thread.Sleep(500);
+        }
+
+        static void Sum()
+        {
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            Console.WriteLine("Сумма " + sum);
+            Thread.Sleep(500);
+        }
+
+        static void Sort()
+        {
+            Array.Sort(array);
+            Console.WriteLine(String.Join(" ", array));
+            Thread.Sleep(500);
+        }
+    }
+}
---
+++ Laboratories320/task 19.01.21/ThreadLock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Laboratories320.task_19._01._21
+{
+    class ThreadLock
+    {
+        static int[] array_1 = new int[10];
+        static int[] array_2 = new int[10];
+        static int[] array_3 = new int[10];
+        static object locker = new object();
+        static void Main()
+        {
+            Thread th1 = new Thread(Random_Ar);
+            Thread th2 = new Thread(Sum);
+            th1.Start();
+            th2.Start();
+        }
+
+        static void Random_Ar()
+        {
+            lock (locker)
+            {
+                Console.WriteLine("Поток 1");
+                Random rnd = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    array_1[i] = rnd.Next(10);
+                    array_2[i] = rnd.Next(10);
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(array_1[i] + ", " + array_2[i]);
+                }
+            }
+        }
+
+        static void Sum()
+        {
+            Console.WriteLine("Поток 2");
+            for (int i = 0; i < 10; i++)
+            {
+                array_3[i] = array_1[i] + array_2[i];
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(array_3[i]);
+            }
+        }
+    }
+}

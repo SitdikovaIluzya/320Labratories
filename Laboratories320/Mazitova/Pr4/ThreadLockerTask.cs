6df1a62ae5192852e18a5d27bfef63af4298662b
Mazitova <77325891+Mazitova@users.noreply.github.com>
2021-01-21 09:22:13 +0300

    changed

--- Laboratories320/Mazitova/Pr4/ThreadLockerTask.cs
+++ Laboratories320/Mazitova/Pr4/ThreadLockerTask.cs
@@ -1,10 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Laboratories320.Mazitova.Pr4
 {
     class ThreadLockerTask
     {
+        static int[] array1 = new int[10];
+        static int[] array2 = new int[10];
+        static int[] array3 = new int[10];
+
+        static object locker = new object();
+
+        public void GetArray()
+        {
+            lock (locker)
+            {
+                Console.WriteLine("-------------");
+                Console.WriteLine();
+                Console.WriteLine("First array");
+                Random random = new Random();
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    array1[i] = random.Next(-100, 100);
+                }
+
+                foreach (var it in array1)
+                {
+                    Console.WriteLine(it + " ");
+                }
+            }
+            lock (locker)
+            {
+                Console.WriteLine("-------------");
+                Console.WriteLine();
+                Console.WriteLine("Second array");
+                Random random = new Random();
+                for (int i = 0; i < array2.Length; i++)
+                {
+                    array2[i] = random.Next(-100, 100);
+                }
+
+                foreach (var it in array2)
+                {
+                    Console.WriteLine(it + " ");
+                }
+            }
+
+        }
+        public void Start()
+        {
+            Task GetArr = new Task(GetArray);
+            Task SummaT = new Task(Summa);
+
+            GetArr.Wait();
+            SummaT.Wait();
+           
+        }
+
+        public void Summa()
+        {
+            lock (locker)
+            {
+                Console.WriteLine("-------------");
+                Console.WriteLine();
+                Console.WriteLine("Sum arrays");
+                for (int i = 0; i < array1.Length && i < array2.Length; i++)
+                {
+                    array3[i] = array1[1] + array2[i];
+                }
+
+                foreach (var item in array3)
+                {
+                    Console.WriteLine(item + " ");
+                }
+            }
+
+        }
     }
 }

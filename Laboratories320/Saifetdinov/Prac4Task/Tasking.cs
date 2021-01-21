eb5d9d152b1b819671f31c533e02306456fea420
Эмиль <77325950+ImilSaifetdinov@users.noreply.github.com>
2021-01-21 09:15:47 +0300

    Finish prac task

--- Laboratories320/Saifetdinov/Prac4Task/Tasking.cs
+++ Laboratories320/Saifetdinov/Prac4Task/Tasking.cs
@@ -9,46 +9,9 @@ namespace Laboratories320.Saifetdinov.Prac4Task
     {
         public static void Tasks()
         {
-            Task task1 = new Task(() => Methods.GenerationElments());
-            task1.Start();
+            Task task1 = Task.Factory.StartNew(() => MethodsArray.Methods.GenerationElments());
+            Task task2 = Task.Factory.StartNew(() => MethodsArray.Methods.SummaArray());
+            task2.Wait();
         }
-        public static class Methods
-        {
-            static int[] array1 = new int[10];
-            static int[] array2 = new int[10];
-            static int[] result = new int[10];
-            static object locker = new object();
-
-            public static void GenerationElments()
-            {
-                lock (locker)
-                {
-                    Random random = new Random();
-                    for (int i = 0; i < array1.Length; i++)
-                    {
-                        array1[i] = random.Next(-10, 10);
-                        array2[i] = random.Next(-10, 10);
-                    }
-                }
-            }
-            public static void SummaArray()
-            {
-                Console.WriteLine("First array");
-                for (int i = 0; i < array1.Length; i++)
-                {
-                    Console.WriteLine(array1[i]);
-                }
-                Console.WriteLine("secod array");
-                for (int i = 0; i < array2.Length; i++)
-                {
-                    Console.WriteLine(array2[i]);
-                }
-                Console.WriteLine("Summa array");
-                for (int i = 0; i < result.Length; i++)
-                {
-                    result[i] = array1[i] + array2[i];
-                    Console.WriteLine(result[i]);
-                }
-            }
-        }
+    }
 }

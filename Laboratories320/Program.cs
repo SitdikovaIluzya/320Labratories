2b4bf672d0d21deb1b07345062eb1b398fa10a3e
Mazitova <77325891+Mazitova@users.noreply.github.com>
2021-01-21 13:14:48 +0300

    changed

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Laboratories320.Mazitova.Pr3;
+using Laboratories320.Mazitova.Pr4;
 using System;
 
 namespace Laboratories320
@@ -12,6 +13,9 @@ namespace Laboratories320
 
             ThreadLocker thr = new ThreadLocker();
             thr.Start();
+
+            ThreadLockerTask task = new ThreadLockerTask();
+            task.Start();
 
         }
     }

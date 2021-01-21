dfb77dbda18ce84b44659899b90c5a73496a0d36
Ranis666 <zakirov.ranis@gmail.com>
2021-01-21 09:14:34 +0300

    Finish 4 practice

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Laboratories320.Zakirov._2Thread;
 using Laboratories320.Zakirov._3ThreadLocker;
+using Laboratories320.Zakirov._4ThreadTask;
 using System.Threading.Tasks;
 
 namespace Laboratories320
@@ -9,11 +10,7 @@ namespace Laboratories320
     {
         static void Main(string[] args)
         {
-            Task task1 = new Task(() => Console.WriteLine("*"));
-            task1.Start();
-            Task task2 = Task.Run(() => Console.WriteLine("#"));
-            task1.Wait();
-            task2.Wait();
+            ThTask.Tasks();
         }
     }
 }
--- Laboratories320/Zakirov/4ThreadTask/ThTask.cs
+++ Laboratories320/Zakirov/4ThreadTask/ThTask.cs
@@ -1,10 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Laboratories320.Zakirov._4ThreadTask
 {
     class ThTask
     {
+        static int[] numbers1 = new int[10];
+        static int[] numbers2 = new int[10];
+        static int[] result = new int[10];
+
+        public static void Tasks()
+        {
+            Task task1 = new Task(() => Generator());
+            Task task2 = new Task(() => Sum());
+            task1.Start();
+            task1.Wait();
+            task2.Start();
+        }
+
+        public static void Generator()
+        {
+                Random rnd = new Random();
+                for (int i = 0; i < numbers1.Length; i++)
+                {
+                    numbers1[i] = rnd.Next(1, 9);
+                    Console.Write(numbers1[i] + " ");
+                }
+
+                Console.WriteLine();
+
+                for (int i = 0; i < numbers2.Length; i++)
+                {
+                    numbers2[i] = rnd.Next(1, 9);
+                    Console.Write(numbers2[i] + " ");
+                }
+                Console.WriteLine();
+        }
+
+        public static void Sum()
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = numbers1[i] + numbers2[i];
+                Console.Write(result[i] + " ");
+            }
+        }
     }
+
 }

ae72d4d3e7be1e1c0864793e989965e016cfc284
Sindryakov <77325774+Sindryakov@users.noreply.github.com>
2021-01-21 12:32:35 +0300

    Start Lesson

---
+++ Laboratories320/Sindryakov/ContinuationTasks/Tasks.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Sindryakov.ContinuationTasks
+{
+    class Tasks
+    {
+        //int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 60,25,34,56,78,89,14,27,18,16,29 };
+        static int[] numbers1 = new int[20];
+    }
+}

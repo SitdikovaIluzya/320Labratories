d89b6377dc0047cdb1b49adcebfda33fb40e2489
CrackAndDie <abdrakovairat@gmail.com>
2021-01-22 10:24:03 +0300

    creating structure

--- Laboratories320/Abdrakov/TPL/TPL1.cs
+++ Laboratories320/Abdrakov/TPL/TPL1.cs
@@ -6,5 +6,23 @@ namespace Laboratories320.Abdrakov.TPL
 {
     class TPL1
     {
+        private static int[] array1 = new int[10];
+        private static int[] array2 = new int[10];
+        private static int[] sumArray = new int[10];
+
+        public static void PseudoMain()
+        {
+
+        }
+
+        private static void RandomFillArray()
+        {
+
+        }
+
+        private static void SumOfArray()
+        {
+
+        }
     }
 }

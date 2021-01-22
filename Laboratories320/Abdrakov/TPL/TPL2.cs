4dfedfec3ca8010f31f0200771ba960ba4cd5c14
CrackAndDie <abdrakovairat@gmail.com>
2021-01-22 10:49:12 +0300

    finish of practice ContinuationTasks

--- Laboratories320/Abdrakov/TPL/TPL2.cs
+++ Laboratories320/Abdrakov/TPL/TPL2.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Laboratories320.Abdrakov.TPL
 {
@@ -11,22 +10,43 @@ namespace Laboratories320.Abdrakov.TPL
 
         public static void PseudoMain()
         {
-
+            Task taskGenerate = new Task(GenerateArray);
+            taskGenerate.Start();
+            taskGenerate.Wait();
+            Task taskProduct = taskGenerate.ContinueWith(prod => FindProduct());
+            Task taskPrint = taskProduct.ContinueWith(res => PrintResult());
+            taskPrint.Wait();
         }
 
         private static void GenerateArray()
         {
-
+            Random random = new Random();
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = random.Next(1, 10);
+                array[i] = random.Next(1, 10);
+            }
+            Console.WriteLine("Array: [{0}]", string.Join(", ", array));
         }
 
         private static void FindProduct()
         {
-
+            for (int i = 0; i < array.Length; ++i)
+                product *= array[i];
+            Console.WriteLine("Product: " + product);
         }
 
         private static void PrintResult()
         {
-
+            string str = product.ToString();
+            Console.Write("Even digits: ");
+            foreach (char digit in str)
+            {
+                int bar = digit - '0';
+                if (bar % 2 == 0)
+                    Console.Write(bar + " ");
+            }
+            Console.WriteLine();
         }
     }
 }

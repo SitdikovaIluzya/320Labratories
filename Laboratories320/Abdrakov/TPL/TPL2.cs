2db590256b7ab3b4a574f1017b4d78ae6f3fd674
CrackAndDie <abdrakovairat@gmail.com>
2021-01-22 10:37:19 +0300

    created structure for 4th pr

--- Laboratories320/Abdrakov/TPL/TPL2.cs
+++ Laboratories320/Abdrakov/TPL/TPL2.cs
@@ -6,5 +6,27 @@ namespace Laboratories320.Abdrakov.TPL
 {
     class TPL2
     {
+        private static int[] array = new int[20];
+        private static long product = 1;
+
+        public static void PseudoMain()
+        {
+
+        }
+
+        private static void GenerateArray()
+        {
+
+        }
+
+        private static void FindProduct()
+        {
+
+        }
+
+        private static void PrintResult()
+        {
+
+        }
     }
 }

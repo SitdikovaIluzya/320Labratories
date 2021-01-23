67c4a25aff29a62a2a8c3488153bc3cece530ac4
Marsel011 <77325786+Marsel011@users.noreply.github.com>
2021-01-23 10:14:29 +0300

    FinishContinTask

--- Laboratories320/Musin/ContinuationTasks/ContinTasks.cs
+++ Laboratories320/Musin/ContinuationTasks/ContinTasks.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Laboratories320.Musin.ContinuationTasks
 {
@@ -21,11 +23,40 @@ namespace Laboratories320.Musin.ContinuationTasks
 
         public static void Multiplier()
         {
+            Thread.Sleep(100);
             for (int i = 0; i < array.Length; i++)
             {
                 result *= array[i];
             }
             Console.WriteLine(result);
+            Console.WriteLine();
+        }
+
+        public static void Parity()
+        {
+            while (result > 10)
+            {
+                if (result % 2 == 0)
+                {
+                    string arr = result.ToString();
+                    Console.Write("|" + arr[arr.Length - 1] + "|");
+                }
+                result /= 10;
+            }
+            if (result % 2 == 0)
+            {
+                Console.Write(result);
+            }
+        }
+
+        public static void Start()
+        {
+            Task task1 = new Task(Generator);
+            Task task2 = new Task(Multiplier);
+            Task task3 = task2.ContinueWith(parity => Parity());
+            task1.Start();
+            task2.Start();
+            task3.Wait();
         }
     }
 }

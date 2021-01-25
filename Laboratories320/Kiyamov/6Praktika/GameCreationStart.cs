bca3960c919e2757ab79c3a0397f278aeb0c6b72
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-25 09:02:55 +0300

    start 6 praktika

---
+++ Laboratories320/Kiyamov/6Praktika/GameCreationStart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Laboratories320.Kiyamov._6Praktika;
+
+namespace Laboratories320.Kiyamov._6Praktika
+{
+    class GameCreationStart
+    {
+        public string UserName { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public int Victories { get; set; }
+        public int GamePoints{ get; set; }
+        public int FinishedMatches { get; set; }
+    }
+}

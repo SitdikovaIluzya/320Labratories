365c5f397ef79d194151d1ebfed5a27e081447bc
Elizkins <semenovaeliza7@gmail.com>
2021-01-25 09:01:01 +0300

    Update 25.01.21

---
+++ Laboratories320/task 25.01.21/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.task_25._01._21
+{
+    class User
+    {
+        string userName;
+        DateTime 
+    }
+}

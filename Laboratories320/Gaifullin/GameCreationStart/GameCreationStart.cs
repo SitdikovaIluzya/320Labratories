b38970d27a350ec75310e26c3aa7abeaf2507f28
@GGRustem <gaifullin.rustem2018@gmail.com>
2021-01-25 09:00:30 +0300

    Gaifullin
    Start Lesson 25.01.2021(GameCreationStart)

---
+++ Laboratories320/Gaifullin/GameCreationStart/GameCreationStart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Gaifullin.GameCreationStart
+{
+    class GameCreationStart
+    {
+
+    }
+}
--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -9,22 +9,42 @@ namespace Laboratories320.Gaifullin
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Leave hope behind all who enters here....");
-            Console.WriteLine();
+            //Console.WriteLine("Leave hope behind all who enters here....");
+            //Console.WriteLine();
 
-            int[] array = new int[20];
-            Random rnd = new Random();
-            for (int i = 0; i < 20; i++)
-            {
-                array[i] = rnd.Next(1, 10);
-            }
-            Console.WriteLine("Generating");
-            Console.WriteLine("[{0}]", string.Join(", ", array));
-            Task task1 = Task.Run(() => PrTask.Сomposition(array));
-            Task task2 = task1.ContinueWith(mul => PrTask.Even());
-            task2.Wait();
+            //int[] array = new int[20];
+            //Random rnd = new Random();
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    array[i] = rnd.Next(1, 10);
+            //}
+            //Console.WriteLine("Generating");
+            //Console.WriteLine("[{0}]", string.Join(", ", array));
+            //Task task1 = Task.Run(() => PrTask.Сomposition(array));
+            //Task task2 = task1.ContinueWith(mul => PrTask.Even());
+            //task2.Wait();
 
-
+            //Parallel.Invoke(sum,nul,div);
         }
+        //static void nul()
+        //{
+        //    Thread.Sleep(2000);
+        //    Console.WriteLine("1 Id: ",Task.CurrentId);
+        //    Console.WriteLine("Result:"+ 5*9);
+
+        //}
+        //static void div()
+        //{
+        //    Thread.Sleep(2000);
+        //    Console.WriteLine("2 Id: ", Task.CurrentId);
+        //    Console.WriteLine("Result:"+ 5/9);
+        //}
+        //static void sum()
+        //{
+        //    Thread.Sleep(2000);
+        //    Console.WriteLine("3 Id: ", Task.CurrentId);
+        //    Console.WriteLine("Result:",+ 5+9);
+        //}
+
     }
 }

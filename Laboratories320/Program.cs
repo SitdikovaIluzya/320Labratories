4412069ae424ceae484c2b883d94bea422472f6d
Elizkins <semenovaeliza7@gmail.com>
2021-01-25 08:52:53 +0300

    Start 25.01.21

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -7,51 +7,10 @@ namespace Laboratories320
 {
     class Program
     {
-        static int[] array = new int[20];
-        static int prod = 1;
+        
         static void Main(string[] args)
         {
-            Task task1 = new Task(() => Random_Ar());
-            task1.Start();
-            Task task2 = task1.ContinueWith(Product);
-            task2.Wait();
-            Task task3 = task2.ContinueWith(EvenDigits);
-            task3.Wait();
-        }
 
-        static void Random_Ar()
-        {
-            Random rnd = new Random();
-            for (int i = 0; i < 20; i++)
-            {
-                array[i] = rnd.Next(1, 5);
-            }
-            Console.WriteLine("Массив = " + String.Join(", ", array));
         }
-
-        static void Product(Task t)
-        {
-            for (int i = 0; i < 20; i++)
-            {
-                prod *= array[i];
-            }
-            Console.WriteLine("Произведение = " + prod);
-        }
-
-        static void EvenDigits(Task t)
-        {
-            string chars = prod.ToString();
-            List<int> result = new List<int> { };
-            for(int i = 0; i < chars.Length; i++)
-            {
-                int a = Convert.ToInt32(new string(chars[i], 1));
-                if (a % 2 == 0)
-                {
-                    result.Add(a);
-                }
-            }
-            Console.WriteLine("Цифры = " + String.Join(", ", result));
-        }
-
     }
 }
--- Laboratories320/task 21.01.21/Tasks2.cs
+++ Laboratories320/task 21.01.21/Tasks2.cs
@@ -1,10 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Laboratories320.task_21._01._21
 {
     class Tasks2
     {
+        static int[] array = new int[20];
+        static int prod = 1;
+
+        static void TaskTwoMain()
+        {
+            Task task1 = new Task(() => Random_Ar());
+            task1.Start();
+            Task task2 = task1.ContinueWith(Product);
+            task2.Wait();
+            Task task3 = task2.ContinueWith(EvenDigits);
+            task3.Wait();
+        }
+
+
+        static void Random_Ar()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                array[i] = rnd.Next(1, 5);
+            }
+            Console.WriteLine("Массив = " + String.Join(", ", array));
+        }
+
+        static void Product(Task t)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                prod *= array[i];
+            }
+            Console.WriteLine("Произведение = " + prod);
+        }
+
+        static void EvenDigits(Task t)
+        {
+            string chars = prod.ToString();
+            List<int> result = new List<int> { };
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int a = Convert.ToInt32(new string(chars[i], 1));
+                if (a % 2 == 0)
+                {
+                    result.Add(a);
+                }
+            }
+            Console.WriteLine("Цифры = " + String.Join(", ", result));
+        }
     }
 }

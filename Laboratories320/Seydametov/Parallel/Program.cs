cf8233971e2d699ebcce1dbda22a73f59ffc9e80
seydametov <eldar.seyametov@mail.ru>
2021-01-25 08:20:19 +0300

    start lesson "Parallel"

---
+++ Laboratories320/Seydametov/Parallel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Laboratories320.Seydametov.Parallel
+{
+    class Program
+    {
+        static void Parallel()
+        {
+            Parallel.Invoke(sum, mul, div);
+        }
+        static void sum()
+        {
+            Console.WriteLine(5 + 9);
+        }
+
+        static void mul()
+        {
+            Console.WriteLine(5 * 9);
+
+        }
+
+        static void div()
+        {
+            Console.WriteLine();
+        }
+    }
+}

6c6bf2480f042b3fc9ef0e6e99095e74313c7293
SitdikovaIluzya <iluza.sitdikova15@mail.ru>
2021-02-08 09:20:05 +0300

    finish

---
+++ Laboratories320/Pract7/Heroes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace Laboratories320.Pract7
+{
+    class Heroes
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        [BsonIgnoreIfDefault]
+        public object HeroesId;
+        [BsonIgnoreIfNull]
+        public string HeroesName { get; set; }
+        [BsonIgnoreIfDefault]
+        public int Health { get; set; }
+
+
+
+        public static async Task HeroesInsert(Heroes hr)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Moovi");
+            var collection = database.GetCollection<Heroes>("Heroes");
+            await collection.InsertOneAsync(hr);
+        }
+
+        public static async Task HeroesNameResault(string heroesName, Heroes newHeroes)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Moovi");
+            var collection = database.GetCollection<Heroes>("Heroes");
+            await collection.ReplaceOneAsync(heroes => heroes.HeroesName == heroesName, newHeroes);
+        }
+    }
+
+    
+}
---
+++ Laboratories320/Pract7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Laboratories320.Pract7;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Leave hope behind all who enters here...");
+            //Console.WriteLine();
+
+            //ThreadLocker thr = new ThreadLocker();
+            //thr.Start();
+
+            //ThreadLockerTask task = new ThreadLockerTask();
+            //task.Start();
+
+            Heroes heroes = new Heroes
+            {
+                HeroesName = "Nolly",
+                Health = 150
+            };
+
+            //Heroes.HeroesInsert(heroes).GetAwaiter().GetResult();
+
+            Heroes heroes1 = new Heroes
+            {
+                HeroesName = "Bill",
+                Health = 99
+            };
+            //Hero.HeroInsert(hero1).GetAwaiter().GetResult();
+            Heroes.HeroesNameResault("H", heroes1).GetAwaiter().GetResult();
+        }
+    }
+}
